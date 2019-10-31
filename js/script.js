cd5dc5af724e3a55d530379d8c64ecd1da1b53c0
Tchore85 <prokopko1@gmail.com>
2019-10-31 16:39:33 -0700

    adding responding loader

--- js/script.js
+++ js/script.js
@@ -3,12 +3,16 @@ document.addEventListener("DOMContentLoaded", function() {
   //   event.preventDefault();
 
   // });
+
   $("form").on("change", "#sections", function(event) {
     let home;
     home = $(this)
       .val()
       .toLowerCase();
     // console.log("heloloooll");
+    $("#ajax-loader").show();
+    console.log($("#ajax-loader"));
+
     $.ajax({
       method: "GET",
       url:
@@ -16,39 +20,46 @@ document.addEventListener("DOMContentLoaded", function() {
         home +
         ".json?api-key=JqxRLlt3a0VlwM6Ag5iAy5VAYiRgvIJg",
       dataType: "json"
-    }).done(function(data) {
-      // console.log(data.results);
+    })
+      .done(function(data) {
+        // console.log(data.results);
 
-      $("#main-content").empty();
-      let filteredResults = data.results
-        .filter(function(article) {
-          return (
-            article.multimedia[4] !== undefined &&
-            article.subsection !== "Art & Design"
-          );
-        })
-        .slice(0, 12);
-      console.log(filteredResults);
-
-      $.each(filteredResults, function(key, value) {
-        // if
-        // (value.multimedia[4].url==undefined || value.multimedia[4].url==null){
-        //   continue;
-        // }
-        $("#main-content").append(
-          `<article class="article" style="background-image:url(${value.multimedia[4].url})">
+        $("#main-content").empty();
+        let filteredResults = data.results
+          .filter(function(article) {
+            return (
+              article.multimedia[4] !== undefined &&
+              article.subsection !== "Art & Design"
+            );
+          })
+          .slice(0, 12);
+        console.log(filteredResults);
+        $.each(filteredResults, function(key, value) {
+          $("#main-content").append(
+            `<article class="article" style="background-image:url(${value.multimedia[4].url})">
           <div class="story">
           <a href="${value.short_url}" target=_blank>
           <p>${value.abstract}</p></a></div></article>`
-        );
-        // $("#main-content").tagsinput({
-        //   maxTags: 12
-        // });
+          );
+        });
+      })
+      .fail(function() {
+        $("#ajax-loader").show();
+      })
+      .always(function() {
+        $("#ajax-loader").hide();
+        console.log("always runs");
       });
-
-      //        <p class="abstr" style="abstrac:(${value.abstract})"></p></div>`
-      // .fail(function() {
-      //   $(".user-name").append("Sorry there was an error.");
+    $(".logo img").css({
+      height: "9rem",
+      transition: "1s ease-in-out"
     });
   });
 });
+
+// const hideSpinner = document.getElementById("sections");
+//       hideSpinner.addEventListener("change", display);
+//       function display() {
+//         console.log("display");
+//         if (hideSpinner === filteredResults) {
+//           conslole.log();
